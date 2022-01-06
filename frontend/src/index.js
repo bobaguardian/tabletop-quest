8956238beaf644b2c028a788ae46c5394d5401e8
Vivian Thach <vivianlthach@gmail.com>
2022-01-06 13:57:04 -0800

    implement session reducer and session actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'
 
 import configureStore from './store';
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
